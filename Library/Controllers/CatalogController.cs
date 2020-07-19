52e3c3678e942baf21f47643078e915220bb4bbc
Ricky Fan <rickyhzfan@gmail.com>
2020-07-18 23:34:18 -0400

    Backup Library Management System

---
+++ Library/Controllers/CatalogController.cs
@@ -0,0 +1,139 @@
+﻿using Library.Models.Catalog;
+using Library.Models.CheckoutModels;
+using LibraryData;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Library.Controllers
+{
+    public class CatalogController : Controller
+    {
+        private ILibraryAsset _assets;
+        private ICheckout _checkouts;
+        public CatalogController(ILibraryAsset assets, ICheckout checkouts)
+        {
+            _assets = assets;
+            _checkouts = checkouts;
+        }
+
+        public IActionResult Index()
+        {
+            var assetModels = _assets.GetAll();
+            var ListingResult = assetModels
+                .Select(result => new AssetIndexListingModel
+                {
+                    Id = result.Id,
+                    ImageUrl = result.ImageUrl,
+                    AuthorOrDirector = _assets.GetAuthorOrDirector(result.Id),
+                    DeweyCallNumber = _assets.GetDeweyIndex(result.Id),
+                    Title = result.Title,
+                    Type = _assets.GetType(result.Id)
+                });
+            var model = new AssetIndexModel()
+            {
+                Assets = ListingResult
+            };
+
+            return View(model);
+        }
+
+        public IActionResult Detail(int id)
+        {
+            var asset = _assets.GetById(id);
+            var currentHolds = _checkouts.GetCurrentHolds(id)
+                .Select(a => new AssetHoldModel
+                {
+                    HoldPlaced = _checkouts.GetCurrentHoldPlaced(a.Id).ToString("d"),
+                    PatronName = _checkouts.GetCurrentHoldPatronName(a.Id),
+                });
+
+            var model = new AssetDetailModel
+            {
+                AssetID = id,
+                Title = asset.Title,
+                Type = _assets.GetType(id),
+                Year = asset.Year,
+                Cost = asset.Cost,
+                Status = asset.Status.Name,
+                ImageUrl = asset.ImageUrl,
+                AuthorOrDirector = _assets.GetAuthorOrDirector(id),
+                CurrentLocation = _assets.GetCurrentLocation(id).Name,
+                DeweyCallNumber = _assets.GetDeweyIndex(id),
+                ISBN = _assets.GetIsbn(id),
+                CheckoutHistory = _checkouts.GetCheckoutHistory(id),
+                LatestCheckout  = _checkouts.GetLatestCheckout(id),
+                PatronName = _checkouts.GetCurrentCheckoutPatron(id),
+                CurrentHolds = currentHolds
+
+            };
+
+            return View(model);
+        }
+        public IActionResult Checkout(int id)
+        {
+            var asset = _assets.GetById(id);
+
+            var model = new CheckoutModel
+            {
+                AssetId = id,
+                ImageUrl = asset.ImageUrl,
+                Title = asset.Title,
+                LibraryCardId = "",
+                IsCheckedOut = _checkouts.IsCheckedOut(id)
+            };
+            return View(model);
+
+        }
+        public IActionResult CheckIn(int id)
+        {
+            _checkouts.CheckInItem(id);
+            return RedirectToAction("Detail", new { id = id });
+        }
+
+        public IActionResult Hold(int id)
+        {
+            var asset = _assets.GetById(id);
+
+            var model = new CheckoutModel
+            {
+                AssetId = id,
+                ImageUrl = asset.ImageUrl,
+                Title = asset.Title,
+                LibraryCardId = "",
+                IsCheckedOut = _checkouts.IsCheckedOut(id),
+                HoldCount = _checkouts.GetCurrentHolds(id).Count()
+            };
+            return View(model);
+
+        }
+
+        public IActionResult MarkLost(int assetId)
+        {
+            _checkouts.MarkLost(assetId);
+            return RedirectToAction("Detail", new { id = assetId });
+        }
+
+        public IActionResult MarkFound(int assetId)
+        {
+            _checkouts.MarkFound(assetId);
+            return RedirectToAction("Detail", new { id = assetId });
+        }
+
+        [HttpPost]
+        public IActionResult PlaceCheckout(int assetId, int LibraryCardId)
+        {
+            _checkouts.CheckOutItem(assetId, LibraryCardId);
+            return RedirectToAction("Detail", new { id = assetId });
+        }
+
+        [HttpPost]
+        public IActionResult PlaceHold(int assetId, int LibraryCardId)
+        {
+            _checkouts.PlaceHold(assetId, LibraryCardId);
+            return RedirectToAction("Detail", new { id = assetId });
+        }
+    }
+}
---
+++ Library/Controllers/PatronController.cs
@@ -0,0 +1,62 @@
+﻿using Library.Models.Patron;
+using LibraryData;
+using LibraryData.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Library.Controllers
+{
+    public class PatronController : Controller
+    {
+        private IPatron _patron;
+        public PatronController(IPatron patron)
+        {
+            _patron = patron;
+        }
+        public IActionResult Index()
+        {
+            var allPatrons = _patron.GetAll();
+
+            var patronModel = allPatrons.Select(p => new PatronDetailModel
+            {
+                Id = p.Id,
+                FirstName = p.FirstName,
+                LastName = p.LastName,
+                LibraryCardId = p.LibraryCard.Id,
+                OverdueFees = p.LibraryCard.Fees,
+                HomeLibraryBranch = p.HomeLibraryBranch.Name
+            }).ToList();
+
+            var model = new PatronIndexModel()
+            {
+                Patrons = patronModel
+            };
+
+            return View(model);
+        }
+
+        public IActionResult Detail(int Id)
+        {
+            var patron = _patron.Get(Id);
+
+            var model = new PatronDetailModel
+            {
+                LastName = patron.LastName,
+                FirstName = patron.FirstName,
+                Address = patron.Address,
+                HomeLibraryBranch = patron.HomeLibraryBranch.Name,
+                MemberSince = patron.LibraryCard.Created,
+                OverdueFees = patron.LibraryCard.Fees,
+                LibraryCardId = patron.LibraryCard.Id,
+                Telephone = patron.TelephoneNumber,
+                AssetCheckedOut = _patron.GetCheckouts(Id).ToList() ?? new List<Checkout>(),
+                CheckoutHistory = _patron.GetCheckoutHistories(Id),
+                Holds = _patron.GetHolds(Id)
+            };
+            return View(model);
+        }
+    }
+}
